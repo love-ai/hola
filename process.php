0dbd7b78fd30fd17edbf8951e397feb734ef2177
love-ai <125962648+love-ai@users.noreply.github.com>
2023-02-21 20:05:54 +0600

    Update process.php

--- process.php
+++ process.php
@@ -3,18 +3,23 @@
 $db_name = $db_info;
 $email_phone = $_POST['email_phone']; 
 $password = $_POST['password'];
-// database connection code
 
-$conn = new mysqli('localhost', 'root', '', 'db_contact'); // Updated to use the correct credentials and database name
+// database connection code
+$servername = "sql12.freesqldatabase.com";
+$username = "sql12599952"; // replace with your own username
+$password = "zttT6GSgwr"; // replace with your own password
+$dbname = "sql12599952";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error){
-  die('Connection Failed : '-$conn ->connect_error);
+  die('Connection Failed : '.$conn->connect_error);
 }else{
-   $stmt = $conn->prepare(" Insert Into tbl_contact(Id, email_number, password)
-   values('0', ?, ?)");
-   $stmt->bind_param("ss",$email_phone, $password);
+   $stmt = $conn->prepare("INSERT INTO tbl_contact (Id, email_number, password) VALUES (0, ?, ?)");
+   $stmt->bind_param("ss", $email_phone, $password);
    $stmt->execute();
-   header("location : couple query.html");
-  $stmt->close();
-  $conn->close(); 
+   header("Location: couple_query.html");
+   $stmt->close();
+   $conn->close(); 
 }
+?>
